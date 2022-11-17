5a9d93943eb9c0a2e379e65040b0d784ccec1ca3
Vinicius Steckelberg <net.vini@gmail.com>
2022-11-17 15:32:49 -0300

    alteracoes de testes

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,17 +2,16 @@ import React from "react";
 import { View, Text, TextInput } from "react-native";
 
 export default function Form() {
-    return (
-
-        <View>
-            <View>
-                <Text>Altura</Text>
-                <TextInput></TextInput>
-                placeholder="Ex. 1,75"
-                <Text>Peso</Text>
-                <TextInput></TextInput>
-            </View>
-
-        </View>
-    );
+  return (
+    <View>
+      <View>
+        <Text>Altura</Text>
+        <TextInput></TextInput>
+        placeholder ="Ex. 1,75"
+        <Text>Peso</Text>
+        <TextInput></TextInput>
+      </View>
+    </View>
+  );
 }
+//eu sou uma 
